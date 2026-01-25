876f8b5152ee3e773dc4cce102db4bc15fbfdd3d
hdinjos <hdinjos@gmail.com>
2026-01-26 00:59:41 +0700

    feat: make migration first

---
+++ drizzle/0000_petite_micromacro.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "dramas" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "dramas_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" varchar(255) NOT NULL,
+	"year" integer NOT NULL,
+	"categoryId" integer,
+	"description" text,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
---
+++ src/database/schemas/dramas.ts
@@ -0,0 +1,18 @@
+import {
+  integer,
+  pgTable,
+  varchar,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const dramas = pgTable('dramas', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  year: integer().notNull(),
+  categoryId: integer(),
+  description: text(),
+  updated_at: timestamp(),
+  created_at: timestamp().defaultNow().notNull(),
+  deleted_at: timestamp(),
+});
