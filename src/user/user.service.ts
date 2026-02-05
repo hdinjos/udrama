7ef43a994ae5025f1d0d78754f229a1967f61c85
Ghufron <groyani464@gmail.com>
2026-02-05 11:34:34 +0700

    feat: make serive user to find user by email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as schema from '../database/schemas';
+import { DrizzleService } from 'src/database/drizzle.service';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly drizzleSerive: DrizzleService) {}
+
+  private get db() {
+    return this.drizzleSerive.db;
+  }
+
+  public findUserByEmail(email: string) {
+    this.db.query.users.findFirst({
+      where: eq(schema.users.email, email),
+    });
+  }
+}
